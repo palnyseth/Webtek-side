034a1ae02647d378f7fbf6ca147f6243f478e036
nyseth <paalnyseth@gmail.com>
2019-02-22 12:20:38 +0100

    Fixed typo

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "TopNav") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
         x.className += " responsive";
     } else {
-        x.className = "TopNav";
+        x.className = "topnav";
     }
 }
